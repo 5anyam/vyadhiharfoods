bb170119148dfed74dfc3f34f4c93e1a83b656c6
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-12-15 22:11:38 +0530

    Add new files

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com","cms.amraj.in","images.remotePatterns"],
+    domains: ["images.unsplash.com","cms.amraj.in"],
   },
   /* config options here */
   reactStrictMode: true,
